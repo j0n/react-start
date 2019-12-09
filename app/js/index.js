ed1f40f5754f781ff041543435af54136645f84f
Jon Andersson <andersson.jon@gmail.com>
2019-12-09 16:03:19 +0100

    Add empty render

--- app/js/index.js
+++ app/js/index.js
@@ -1,1 +1,11 @@
-console.log('yo');
+import React, { Component } from 'react';
+import { render } from 'react-dom';
+
+export default class Index extends Component {
+  render() {
+    return (
+      <div>hej hej</div>
+    );
+  }
+}
+render(<Index />, document.getElementById('app'));
